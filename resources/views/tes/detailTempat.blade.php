6c0256d17bc230ff71f654aafb1aebbc78a36a5e
Muhammad Rieki Hredaya <hredayaa@gmail.com>
2023-04-26 19:11:37 +0700

    card gg jadi

--- resources/views/tes/detailTempat.blade.php
+++ resources/views/tes/detailTempat.blade.php
@@ -90,8 +90,8 @@
 
             <div class="col-md-4">
                 <div class="container-md">
-                    <h1 class="card-text text-center mt-5 pt-3">Ulasan</h1>
-                    <div class="card shadow mt-5 pb-3">
+                    <h1 class="card-text text-end mt-5 pt-3">Ulasan</h1>
+                    <div class="card shadow mt-3 pb-3">
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-md-8">
